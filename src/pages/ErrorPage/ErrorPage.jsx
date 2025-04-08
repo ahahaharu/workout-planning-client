be46d170f5d23f9cb361efcdd21a63bdee945502
Andrey <112519966+ahahaharu@users.noreply.github.com>
2025-04-08 16:44:11 +0300

    error page done

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import PageLayout from "../../components/PageLayout/PageLayout";
-import { Ban } from "lucide-react";
+import { Ban, MoveLeft } from "lucide-react";
+import { Link } from "react-router-dom";
 
 export default function ErrorPage() {
   return (
     <PageLayout>
       <div className="h-full flex justify-center items-center gap-5 flex-col">
         <Ban size={200} className="stroke-indigo-500" />
-        <h1 className="text-indigo-500 text-5xl font-extrabold">
+        <h1 className="text-indigo-500 text-5xl font-extrabold text-center">
           Страницы не существует
         </h1>
+        <p className="text-gray-500 text-lg mt-4 text-center">
+          Вероятно, вы ввели неправильный адрес или страница была удалена.
+        </p>
+        <Link
+          className="text-gray-500 flex gap-4 font-bold underline underline-offset-10"
+          to={"/"}
+        >
+          <MoveLeft />
+          Вернуться на страницу тренировок
+        </Link>
       </div>
     </PageLayout>
   );
