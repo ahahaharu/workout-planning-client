41e4df9a8e00836a74801e2e31a0a01d6c793ca8
Andrey <112519966+ahahaharu@users.noreply.github.com>
2025-04-11 17:52:11 +0300

    modal window for exercise done

--- src/pages/ExercisesPage/ExercisesPage.jsx
+++ src/pages/ExercisesPage/ExercisesPage.jsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import PageLayout from "../../components/PageLayout/PageLayout";
 import CategoryFilter from "../../components/CategoryFilter/CategoryFilter";
 import image from "../../assets/image.png";
-import { Button, Divider } from "antd";
+import { Button, Divider, Modal, Tabs } from "antd";
+import { Edit, Trash } from "lucide-react";
 
 export default function ExercisesPage() {
   const exerciseCategories = [
@@ -22,8 +23,9 @@ export default function ExercisesPage() {
 
   const [selectedCategories, setSelectedCategories] = useState([]);
   const [selectedBodyParts, setSelectedBodyParts] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(true);
 
-  // Here you would filter your exercises based on the selected categories and body parts
   const handleCategorySelect = (categories) => {
     setSelectedCategories(categories);
     console.log("Selected categories:", categories);
@@ -33,6 +35,100 @@ export default function ExercisesPage() {
     setSelectedBodyParts(bodyParts);
     console.log("Selected body parts:", bodyParts);
   };
+
+  const showLoading = () => {
+    setOpen(true);
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  };
+
+  const onChange = (key) => {
+    console.log(key);
+  };
+  const items = [
+    {
+      key: "1",
+      label: "Информация",
+      children: (
+        <div className="flex flex-col gap-4">
+          <div className="rounded-xl overflow-hidden">
+            <img
+              src={image}
+              alt="image"
+              className="w-full h-full object-cover"
+            />
+          </div>
+          <h1 className="text-3xl font-bold">Жим лёжа</h1>
+          <div className="text-gray-600">
+            <p>
+              <b>Категория:</b> Силовые
+            </p>
+            <p>
+              <b>Часть тела: </b>Грудь
+            </p>
+          </div>
+          <div className="mb-5">
+            <p>
+              <b>Описание:</b>
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente
+              aperiam, numquam fugit eum molestiae id molestias beatae
+              voluptatibus voluptatum voluptatem distinctio quidem assumenda
+              iste aliquid maiores ratione fuga tempore veritatis.
+            </p>
+          </div>
+        </div>
+      ),
+    },
+    {
+      key: "2",
+      label: "Демонстрация",
+      children: (
+        <div className="flex flex-col gap-4 mb-4">
+          <h1 className="text-2xl font-bold">Видео демострации упражнения</h1>
+          <iframe
+            src={`https://www.youtube.com/embed/SCVCLChPQFY`}
+            className="w-full aspect-video rounded-xl"
+            title="Жим лёжа демонстрация"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+      ),
+    },
+    {
+      key: "3",
+      label: "История",
+      children: (
+        <div className="flex flex-col gap-4 mb-4">
+          <h1 className="text-2xl font-bold">История выполнения упражнения</h1>
+          <p className="text-center">История пуста</p>
+        </div>
+      ),
+    },
+    {
+      key: "4",
+      label: "Настройки",
+      children: (
+        <div className="mb-4">
+          <h1 className="text-2xl font-bold">Настройки</h1>
+          <div className="flex flex-col gap-4 mt-6">
+            <Button type="primary" size="large">
+              <Edit size={20} />
+              Править упражнение
+            </Button>
+            <Button color="red" variant="outlined" size="large">
+              <Trash size={20} />
+              Удалить упражнение
+            </Button>
+          </div>
+        </div>
+      ),
+    },
+  ];
 
   return (
     <PageLayout title="Упражнения">
@@ -49,7 +145,7 @@ export default function ExercisesPage() {
         />
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 xl:grid-cols-2 gap-4">
         <div className="flex justify-between w-full p-4 border rounded-xl border-indigo-300">
           <div className="flex gap-4">
             <div className="w-22 h-22 rounded overflow-hidden">
@@ -65,7 +161,27 @@ export default function ExercisesPage() {
             </div>
           </div>
           <div className="flex flex-col gap-2">
-            <Button size="large">Информация</Button>
+            <Button size="large" onClick={showLoading}>
+              Информация
+            </Button>
+            <Modal
+              title={<p>Информация об упражнении</p>}
+              footer={
+                <div className="flex gap-4 justify-end">
+                  <Button onClick={() => setOpen(false)}>
+                    Добавить заметку
+                  </Button>
+                  <Button type="primary" onClick={() => setOpen(false)}>
+                    Добавить в программу тренировок
+                  </Button>
+                </div>
+              }
+              loading={loading}
+              open={open}
+              onCancel={() => setOpen(false)}
+            >
+              <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
+            </Modal>
             <Button size="large" type="primary">
               Добавить в программу
             </Button>
